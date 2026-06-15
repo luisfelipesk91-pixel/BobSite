1e25ea032e4c68124ce6a0423053e0c3988fea1c
luisfelipesk91-pixel <luisfelipesk91@gmail.com>
2026-06-15 04:46:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -627,6 +627,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// CORS para permitir requisições do Vercel
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", FRONTEND_URL);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-admin-secret, x-client, x-script-secret, x-railway-secret");
+    res.header("Access-Control-Allow-Credentials", "true");
+    
+    // Handle preflight
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
